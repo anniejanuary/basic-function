2b7008a0260362de779b52b5fdc733fc72863112
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 11:16:00 +0200

    Create basic-function.py

---
+++ basic-function.py
@@ -0,0 +1,11 @@
+a=17
+print(a)
+
+def calculator(a,b):
+    print("Sum of a and b =",a+b)
+    print("Substraction of a and b =",a-b)
+    print("Multiplication of a and b =",a*b)
+    print("Division of a and b =",a/b)
+calculator(10,5)
+
+print(a)
